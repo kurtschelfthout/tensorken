96a05e901673e1fdfb9161e1b7a00a4991763f6f
Alex Iadicicco <alex@ajitek.net>
2026-04-26 13:53:00 -0700

    Add naive conv2d to the benchmarks

--- benches/tensor_conv2d.rs
+++ benches/tensor_conv2d.rs
@@ -18,6 +18,11 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             let im_cpu = Cpu32::from(&im_gpu);
             let ker_cpu = Cpu32::from(&ker_gpu);
 
+            let im_array_buffer: Vec<f32> = im_cpu.ravel();
+            let ker_array_buffer: Vec<f32> = ker_cpu.ravel();
+            let im_array: Vec<&[f32]> = im_array_buffer.chunks_exact(im_size).collect();
+            let ker_array: Vec<&[f32]> = ker_array_buffer.chunks_exact(ker_size).collect();
+
             group.bench_with_input(
                 BenchmarkId::new(format!("cpu {ker_size}x{ker_size}"), im_size),
                 &im_size,
@@ -28,9 +33,37 @@ pub fn criterion_benchmark(c: &mut Criterion) {
                 &im_size,
                 |b, _| b.iter(|| black_box(im_gpu.conv2d(&ker_gpu).realize())),
             );
+            group.bench_with_input(
+                BenchmarkId::new(format!("naive {ker_size}x{ker_size}"), im_size),
+                &im_size,
+                |b, _| b.iter(|| black_box(naive_conv2d(&im_array[..], &ker_array[..]))),
+            );
         }
     }
     group.finish();
+}
+
+fn naive_conv2d(im: &[&[f32]], ker: &[&[f32]]) -> Vec<Vec<f32>> {
+    let ih = im.len();
+    let iw = im[0].len();
+    let kh = ker.len();
+    let kw = ker[0].len();
+    let oh = ih - kh + 1;
+    let ow = iw - kw + 1;
+
+    let mut out = vec![vec![0.0; ow]; oh];
+
+    for n in 0..oh {
+        for m in 0..ow {
+            for i in 0..kh {
+                for j in 0..kw {
+                    out[n][m] += ker[i][j] * im[n + i][m + j];
+                }
+            }
+        }
+    }
+
+    out
 }
 
 criterion_group!(benches, criterion_benchmark);
